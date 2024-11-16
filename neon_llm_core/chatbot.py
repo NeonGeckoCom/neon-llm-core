bb1c9a85af4198278354d3691f3b2cff54989854
Daniel McKnight <daniel@neon.ai>
2024-11-15 18:48:38 -0800

    WIP implementation of `neon_data_models` for MQ message validation

--- neon_llm_core/chatbot.py
+++ neon_llm_core/chatbot.py
@@ -211,6 +211,12 @@ class LLMBot(ChatBot):
         :param responses: list of answers to select from
         :returns response data from LLM API
         """
+        request_data = LLMVoteRequest(model=self.base_llm,
+                                      persona=self.persona,
+                                      query=prompt,
+                                      responses=responses,
+                                      history=[],
+                                      message_id="")
         queue = self.mq_queue_config.ask_appraiser_queue
         response_queue = f"{queue}.response.{uuid4().hex}"
 
